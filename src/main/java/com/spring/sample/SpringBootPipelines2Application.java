0f53c0034dd47e181d47905d22ac98460ef2e1f7
MuseGlobal <aurelian.popescu@museglobal.ro>
2023-01-11 10:40:53 +0200

    dss

--- src/main/java/com/spring/sample/SpringBootPipelines2Application.java
+++ src/main/java/com/spring/sample/SpringBootPipelines2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootPipelines2Application {
 
 	public static void main(String[] args) {
+	  System.out.println("ddda");
 		SpringApplication.run(SpringBootPipelines2Application.class, args);
 	}
 
