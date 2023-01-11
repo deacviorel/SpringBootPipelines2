23fd0d0d6717d2063222273a435d30de5c52a6e7
MuseGlobal <aurelian.popescu@museglobal.ro>
2023-01-11 10:06:17 +0200

    second

---
+++ src/main/java/com/spring/sample/SpringBootPipelines2Application.java
@@ -0,0 +1,13 @@
+package com.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPipelines2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPipelines2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/sample/SpringBootPipelines2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPipelines2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
